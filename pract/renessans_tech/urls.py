b3a2cf7d5316fa6cf710efed59c597495581749f
MargaritaUsova <askacziaa@gmail.com>
2023-06-19 17:03:30 +0300

    new adds to css commit

--- pract/renessans_tech/urls.py
+++ pract/renessans_tech/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index, name = 'main'),
+    path('', views.index, name = 'index'),
     path('about-us', views.about, name = 'about'),
     path('create', views.create, name = 'create'),
     path('phones', views.phones),
