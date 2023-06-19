fba934c658c58afdcaffa13e8eeeee969567c74d
MargaritaUsova <askacziaa@gmail.com>
2023-06-19 23:31:19 +0300

    phone names + prices from json to html commit

--- pract/renessans_tech/templates/renessans_tech/phones_html.py
+++ pract/renessans_tech/templates/renessans_tech/phones_html.py
@@ -1,15 +1,18 @@
 import json
 
-with open('/Users/margaritausova/Documents/pract/phones_data.json') as f:
-    templates = json.load(f)
+with open('/Users/margaritausova/Documents/pract/phone_prices.json') as f1:
+    prices = json.load(f1)
+
 
 with open('phones.html', 'a') as f:
-
-    for i in templates:
-        f.write("""    <h1>{link}</h1>
-        """.format(link=templates[i]['item_name']))
+    #print(prices['30067439']['item_base_price'])
+    for i in prices:
+        f.write("""    <h1>{price}</h1> <h2>{name}</h2><h3> )</h3>
+        """.format(price=prices[i]['item_discount_price'],
+                   name = prices[i]['item_name']))
     f.write("""</body>
     </html>""")
+
 
 
 
