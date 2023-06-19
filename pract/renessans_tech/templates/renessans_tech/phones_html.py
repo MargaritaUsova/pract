2adc66c4597bee5c5f3d3a3ec5a8c4d0371d70aa
MargaritaUsova <askacziaa@gmail.com>
2023-06-19 22:44:38 +0300

    all links of header commit

---
+++ pract/renessans_tech/templates/renessans_tech/phones_html.py
@@ -0,0 +1,17 @@
+import json
+
+with open('/Users/margaritausova/Documents/pract/phones_data.json') as f:
+    templates = json.load(f)
+
+with open('phones.html', 'a') as f:
+
+    for i in templates:
+        f.write("""    <h1>{link}</h1>
+        """.format(link=templates[i]['item_name']))
+        print(templates[i]['item_name'])
+    f.write("""</body>
+    </html>""")
+
+
+
+
